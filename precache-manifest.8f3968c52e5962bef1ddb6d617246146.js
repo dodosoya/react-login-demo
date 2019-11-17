1c644c49eea51ee4d872d28abe046e558fc7db59
Ryan Hsieh <chengyu0827@gmail.com>
2019-11-17 14:09:49 +0800

    Updates

---
+++ precache-manifest.8f3968c52e5962bef1ddb6d617246146.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3e42c73b67675cdecf30d03e23c5de",
+    "url": "/react-login-demo/index.html"
+  },
+  {
+    "revision": "f6bdc3a486b95cb63fb5",
+    "url": "/react-login-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2dca58b589d10d21cbdd",
+    "url": "/react-login-demo/static/js/2.cc163324.chunk.js"
+  },
+  {
+    "revision": "f6bdc3a486b95cb63fb5",
+    "url": "/react-login-demo/static/js/main.861b849c.chunk.js"
+  },
+  {
+    "revision": "1b756e342631fae01cd1",
+    "url": "/react-login-demo/static/js/runtime-main.328f5bc7.js"
+  }
+]);
